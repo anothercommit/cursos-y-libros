c6ee6f209da41e511e7c647d27d7a9fb4a81b7e5
Your Name <you@example.com>
2024-08-07 12:58:25 -0300

    Wed Aug  7 12:58:24 PM -03 2024

--- The C Programming Language/1-16.c
+++ The C Programming Language/1-16.c
@@ -17,15 +17,15 @@ int main() {
       copy(longest, line);
     }
 
-  if (max > 0) /* there was a line */
-    printf("%s", longest);
+  if (max > 0)
+    printf("%d: %s\n", len, longest);
 
   return 0;
 }
 
 int my_getline(char s[], int lim) {
   int c, i;
-  for (i = 0; (c = getchar()) != EOF && c != '\n'; ++i)
+  for (i = 0; (c = getchar()) != EOF && c != '\n' && i < lim - 2; ++i)
     if (i < lim - 1)
       s[i] = c;
   if (c == '\n') {
