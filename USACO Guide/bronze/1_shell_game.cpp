bd390166660bbfbe565b34bb17d8aa1e333e3875
Your Name <you@example.com>
2024-10-04 13:42:30 -0300

    Fri Oct  4 01:42:30 PM -03 2024

--- USACO Guide/bronze/1_shell_game.cpp
+++ USACO Guide/bronze/1_shell_game.cpp
@@ -1,1 +1,31 @@
-int main() {}
+#include <algorithm>
+#include <array>
+#include <bits/stdc++.h>
+#include <utility>
+#include <vector>
+using namespace std;
+
+vector<int> shells{0, 1, 2};
+array<int, 3> counter;
+
+int main() {
+  fill_n(counter.begin(), counter.size(), 0);
+  int N;
+  cin >> N;
+
+  shells[0] = true;
+  shells[1] = true;
+  shells[2] = true;
+
+  int a, b, g;
+  for (int _ = 0; _ < N; ++_) {
+    cin >> a >> b >> g;
+    --a, --b, --g;
+
+    swap(shells[a], shells[b]);
+
+    ++counter[shells[g]];
+  }
+
+  cout << max({counter[0], counter[1], counter[2]}) << endl;
+}
