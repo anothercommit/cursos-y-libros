c8bc9d449ad816da5de71749d036ca528d3489eb
Tu nombre <you@example.com>
2024-12-30 14:19:30 -0300

    lunes, 30 de diciembre de 2024, 14:19:30 -03

---
+++ Campamento Iberoamericano 2024-2025/search_peek.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define ll long long
+#define ull unsigned long long
+#define fi first
+#define se second
+#define spa << ' ' <<
+using namespace std;
+
+int arr[] = {1, 2, 3, 4, 5, 4, 3, 2, 1};
+
+void solve() {
+    int l = 0, r = 8;
+
+    int mid = (l + r) / 2;
+
+    while (l < r) {
+        arr[mid] < arr[mid + 1] //
+            ? l = mid + 1
+            : r = mid;
+    }
+
+    cout << l << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+
+    cout << '\n';
+
+    while (t--) {
+        solve();
+    }
+}
