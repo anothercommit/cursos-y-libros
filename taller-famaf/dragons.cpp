8712ce523f3a733f8bc2caa1ecbdd94a5a39065a
anothercommit <lack_of_names@proton.me>
2025-05-15 08:46:53 -0300

    Thu May 15 08:46:53 AM -03 2025

---
+++ taller-famaf/dragons.cpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+#define fore(i, a, b) for (ll i = a; i < b; ++i)
+#define rep(i, b) for (ll i = o; i < b; ++i)
+#define sz(a) ((int)a.size())
+#define all(x) (x).begin(), (x).end()
+#define spa << ' ' <<
+#define fi first
+#define se second
+#define endl '\n'
+
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef vector<ll> vv;
+typedef vector<db> vd;
+typedef pair<ll,ll> pp;
+
+bool ordenar(pair<int, int> a, pair<int, int> b){
+    return a.fi < b.fi;
+}
+
+void solve() {
+    ll s, n; cin >> s >> n;
+    vector<pp> dragons (n);
+
+    fore(i, 0, n){
+        cin >> dragons[i].fi >> dragons[i].se;
+    }
+
+    sort(all(dragons), ordenar);
+
+    for (pp dragon : dragons){
+        if (s > dragon.first) s += dragon.se;
+        else {
+            cout << "NO" << endl;
+            return;
+        }
+    }
+
+    cout << "YES" << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int t = 1;
+    // cin >> t;
+
+    cout << '\n';
+
+    while (t--) {
+        solve();
+    }
+
+    return EXIT_SUCCESS;
+}
