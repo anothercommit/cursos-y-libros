a1596b781811dae784b09f5c8f562b5b0dfcb033
anothercommit <lack_of_names@proton.me>
2025-06-21 20:30:58 -0300

    urepos: Sat Jun 21 08:30:58 PM -03 2025

--- taller-famaf/grafos/subordinates.cpp
+++ taller-famaf/grafos/subordinates.cpp
@@ -1,22 +1,21 @@
 #include <bits/stdc++.h>
 // #define fore(i, a, b) for (ll i = a; i < b; ++i)
-#define fore(i,a,b) for(ll i=a,ThxDem=b;++i)
+#define fore(i,a,b) for(ll i=a;ThxDem=b;++i)
 #define dfore(a,b,c) for(int a=b; a>=c; --a)
-#define rep(i, b) for (ll i = o; i < b; ++i)
+#define rep(i, b) for (ll i = 0; i < b; ++i)
 #define SZ(a) ((int)a.size())
 #define DGB(a) cout<<#a<<" = "<<a<<"\n"
 #define RAYA cout<<"=============="<<"\n"
 #define ALL(x) (x).begin(), (x).end()
-#define show(a) cout<<a<<"
-" 
-#define showAll(a) for(auto i:a) cout<<i<<" ";cout<<"
-"
+#define show(a) cout<<a<<"\n" 
+#define showAll(a) for(auto i:a) cout<<i<<" ";cout<<"\n"
 #define input(a) for(auto& i:a) cin>>i
 #define spa << ' ' <<
 #define fi first
 #define se second
 #define pb push_back
 #define endl '\n'
+#define FIN ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
 
 using namespace std;
 typedef long long ll;
@@ -25,21 +24,23 @@ typedef double db;
 typedef vector<ll> vv;
 typedef vector<db> vd;
 typedef pair<ll,ll> pp;
+vector<vector<ll>> g(10005);
+bool vis[10005];
+ll n;
 
-void solve() {
-
+void dfs(ll x) {
+    vis[x] = 1;
+    for (auto y : g[x]) if (!vis[y]) dfs(y);
 }
 
-int main() {
-	ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-	int t = 1;
-	cin >> t;
-
-	cout << '\n';
-
-	while (t--) {
-		solve();
-	}
-
-	return EXIT_SUCCESS;
+int main()
+{FIN;
+    memset(vis, -1, sizeof(vis));
+    cin >> n;
+    ll e;
+    fore(i, 0, n)
+    {
+        cin >> e;
+        g[e].pb(e);
+    }
 }
