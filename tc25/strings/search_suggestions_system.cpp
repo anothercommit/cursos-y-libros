00ff5a491856c1cdac010a1dcee5c2cf07746415
anothercommit <lack_of_names@proton.me>
2025-08-15 09:11:36 -0300

    urepos: Fri Aug 15 09:11:36 AM -03 2025

---
+++ tc25/strings/search_suggestions_system.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#define forr(i,a,n) for(ll i=a,ThxDem=n;i<ThxDem;++i)
+#define forn(i, n) forr(i,0,n)
+#define dfor(a,b,c) for(int a=b; a>=c; --a)
+#define fill(a) for(auto &nashe: a) cin >> nashe;
+#define filln(a,n) forr(i, 0, n) cin >> a[i];
+#define SZ(a) ((int)a.size())
+#define DGB(a) cout<<#a<<" = "<<a<<"\n"
+#define RAYA cout<<"=============="<<"\n"
+#define ALL(x) (x).begin(), (x).end()
+#define show(a) cout<<a<<"\n" 
+#define showAll(a) for(auto i:a) cout<<i<<" ";cout<<"\n"
+#define input(a) for(auto& i:a) cin>>i
+#define spa << ' ' <<
+#define fi first
+#define se second
+#define pb push_back
+#define nl <<'\n'
+#define FIN ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<ll> vv;
+typedef vector<ld> vd;
+typedef pair<ll,ll> pp;
+
+void solve() {
+	
+}
+
+int main() {FIN;
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return EXIT_SUCCESS;
+}
